70a52a35a4953366fc1830ea12461ac1a2bfe457
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2018-07-27 15:45:43 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -4,6 +4,28 @@ set -x
 
 date
 
+postgres_user=$(echo ${DATABASE_URL} | awk -F':' '{print $2}' | sed -e 's/\///g')
+postgres_password=$(echo ${DATABASE_URL} | grep -o '/.\+@' | grep -o ':.\+' | sed -e 's/://' | sed -e 's/@//')
+postgres_server=$(echo ${DATABASE_URL} | awk -F'@' '{print $2}' | awk -F':' '{print $1}')
+postgres_dbname=$(echo ${DATABASE_URL} | awk -F'/' '{print $NF}')
+
+echo ${postgres_user}
+echo ${postgres_password}
+echo ${postgres_server}
+echo ${postgres_dbname}
+
+export PGPASSWORD=${postgres_password}
+
+#psql -U ${postgres_user} -d ${postgres_dbname} -h ${postgres_server} > /tmp/sql_result.txt << __HEREDOC__
+#CREATE TABLE t_pattern (
+# pattern_id int primary key
+#,preg_match_pattern character varying(255) NOT NULL
+#,preg_replace_pattern character varying(255)
+#,replacement character varying(255)
+#);
+#__HEREDOC__
+#cat /tmp/sql_result.txt
+
 # ***** phppgadmin *****
 
 pushd www
