5ea4aa1c4c3e32b5b23e22847c714ff11a13e69d
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2018-07-27 15:46:01 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -4,6 +4,8 @@ set -x
 
 date
 
+chmod 755 ./start_web.sh
+
 postgres_user=$(echo ${DATABASE_URL} | awk -F':' '{print $2}' | sed -e 's/\///g')
 postgres_password=$(echo ${DATABASE_URL} | grep -o '/.\+@' | grep -o ':.\+' | sed -e 's/://' | sed -e 's/@//')
 postgres_server=$(echo ${DATABASE_URL} | awk -F'@' '{print $2}' | awk -F':' '{print $1}')
@@ -33,7 +35,5 @@ git clone --depth 1 https://github.com/phppgadmin/phppgadmin.git phppgadmin
 cp ../config.inc.php phppgadmin/conf/
 cp ../Connection.php phppgadmin/classes/database/
 popd
-
-chmod 755 ./start_web.sh
 
 date
