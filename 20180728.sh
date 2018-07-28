d62585e3036c987f6ed07c9852e6a8e25dc3ea7b
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2018-07-28 23:32:29 +0900

    Update 20180728.sh

--- 20180728.sh
+++ 20180728.sh
@@ -2,8 +2,16 @@
 
 set -x
 
-url = ${URL01}
+flag='ON'
+url=${URL01}
 while read -r LINE
 do
-  curl -u ${U1}:${P1} ${URL01}${LINE}/
+  if [ $flag = 'ON' ]; then
+    curl -u ${U1}:${P1} ${URL01}${LINE}/ &
+    flag='OFF'
+  else
+    curl -u ${U1}:${P1} ${URL01}${LINE}/
+    flag='ON'
+    wait
+  fi
 done < /tmp/data.txt
